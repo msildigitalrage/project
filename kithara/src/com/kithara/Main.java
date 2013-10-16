7593e485520375d7322e7aa877e17dd0c79d2275
thomas <thomas.zafeiropoulos@gmail.com>
2013-10-16 20:21:07 +0100

    aaaaa

---
+++ kithara/src/com/kithara/Main.java
@@ -0,0 +1,14 @@
+package com.kithara;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Patataaaa");
+
+	}
+
+}
