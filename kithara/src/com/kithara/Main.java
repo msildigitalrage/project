4a30dfa82e82310b6a40881dae342c81aafa969a
thomas <thomas.zafeiropoulos@gmail.com>
2013-10-18 10:03:35 +0100

    Gui.class Frame Added

--- kithara/src/com/kithara/Main.java
+++ kithara/src/com/kithara/Main.java
@@ -7,8 +7,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi Patataaaaaaaa");
-
+		//System.out.println("Main Run");
+		Gui gui = new Gui();
+		
+		
 	}
 
 }
