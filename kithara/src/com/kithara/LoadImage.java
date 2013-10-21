f9407db3af8b88131a408feabffd448996e4196a
thomas <thomas.zafeiropoulos@gmail.com>
2013-10-21 13:22:36 +0100

    pare

---
+++ kithara/src/com/kithara/LoadImage.java
@@ -0,0 +1,30 @@
+package com.kithara;
+
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+public class LoadImage {
+
+	JDialog jDialog = new JDialog();
+	JPanel descPanel = new JPanel(new GridBagLayout());
+	
+	public void newMount(){
+		
+
+	    System.out.print("assa");
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
