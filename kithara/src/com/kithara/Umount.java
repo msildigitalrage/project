f097bffd85a38392cd24929d9b2d1c90836ed209
thomas <thomas.zafeiropoulos@gmail.com>
2013-10-26 16:43:01 +0100

    umount

---
+++ kithara/src/com/kithara/Umount.java
@@ -0,0 +1,47 @@
+package com.kithara;
+
+
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+
+public class Umount {
+	public static String mountPath;
+	
+	public void umount (){
+		
+		 try{
+			 	 
+			  
+			  Process p2= Runtime.getRuntime().exec( "sudo umount "+mountPath);
+			  p2.waitFor();
+					  Gui.unloadAnImage.setEnabled(false);
+					  Gui.loadImage.setEnabled(true);
+					  TreeView.fileTree.setModel(null);
+					  LoadImage.loadedImage=false;
+				  try{
+					  
+					  Process p= Runtime.getRuntime().exec( "sudo rmdir "+mountPath);
+					  p.waitFor();
+					 	  		 
+					  
+				  }
+				  catch(IOException | InterruptedException e1){
+					  
+				  } 
+			  
+		  }
+		  catch(IOException | InterruptedException e1){
+			  
+		  }
+		if(LoadImage.isMounted(mountPath)){
+			JOptionPane.showMessageDialog(null, "something went wrong");
+		}
+		else{
+			System.out.println(mountPath);
+			//JOptionPane.showMessageDialog(null, "the umount held with success");
+		}
+	}	
+
+}
